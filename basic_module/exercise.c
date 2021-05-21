4e06c32e9480faa214abc13041508587ddbc4ae7
Andrzej Telszewski <andrzej@telszewski.com>
2021-05-21 15:24:32 +0200

    Add module info entries (MODULE_*)

--- basic_module/exercise.c
+++ basic_module/exercise.c
@@ -3,8 +3,13 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
+/* Note: Module info entries are only refreshed after module is installed
+ * in /lib/modules and dempmod-ed. They are not available after sole insmod. */
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrzej Telszewski");
+MODULE_DESCRIPTION("Cool module because it does nothing");
+MODULE_VERSION("1.2.3");
 
 static int __init exercise_init(void)
 {
